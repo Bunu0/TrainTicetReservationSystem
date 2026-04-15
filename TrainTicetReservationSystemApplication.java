01dc33398ace96252ab7735019320a40a1d420b0
Bimalendu Bilash Prusty <131747247+Bunu0@users.noreply.github.com>
2026-04-15 23:17:09 +0530

    Add files via upload

---
+++ TrainTicetReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.techm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainTicetReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainTicetReservationSystemApplication.class, args);
+	}
+
+}
